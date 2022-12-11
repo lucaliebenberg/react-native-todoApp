cfb999c9e8e43c404260c54a5fb047aef3fd8757
Luca Liebenberg <liebenbergluca@gmail.com>
2022-12-11 10:19:04 +0200

    Initial commit

---
+++ assets/colors/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+  black: "#000",
+  grey: "#666666",
+  darkGrey: "#D1D3D2",
+  blue: "#1C215B",
+  blueFaded: "#8D8FAD",
+  orange: "#F35D38",
+  white: "#FFFFFF",
+};
+
+export default colors;
